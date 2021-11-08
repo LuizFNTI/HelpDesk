84707526f60fa7564f08d9d0991cfeeb877ae189
Luiz Felipe do Nascimento <felllipppe.nascimento@gmail.com>
2021-11-08 11:02:20 -0300

    Update atualizarDadosUsuarios.php

--- helpdesk/Backend/atualizarDadosUsuarios.php
+++ helpdesk/Backend/atualizarDadosUsuarios.php
@@ -9,31 +9,16 @@ if(isset($_POST['nome'])) {
     $telefone = $_POST['telefone'];
     $departamento = $_POST['departamento'];
 
-    $query = $conn->prepare("UPDATE usuarios SET nome = ?, telefone = ?, email = ?, departamento = ? WHERE matricula = ?");
-    $query->execute(array($nome, $email, $telefone, $departamento, $matricula));
+    $query = $conn->prepare("UPDATE usuarios SET nome = :n, telefone = :n, email = :e, departamento = :d WHERE matricula = :m");
 
-    //$query->bindValue(":n",$nome);
-    //$query->bindValue(":t",$telefone);
-    //$query->bindValue(":e",$email);
-    //$query->bindValue(":d",$departamento);
-    //$query->bindValue(":m", $matricula);
-    //$query->execute();
-    //$query->execute(array(':n' => $nome, ':e' => $email, ':t' => $telefone, ':d' => $departamento, ':m' => $matricula));
+    $query->bindValue(":n",$nome);
+    $query->bindValue(":t",$telefone);
+    $query->bindValue(":e",$email);
+    $query->bindValue(":d",$departamento);
+    $query->bindValue(":m", $matricula);
+    $query->execute();
 }
 
-
-//header("location: ../Frontend/gerenciarUsuarios.php");
+header("location: ../Frontend/gerenciarUsuarios.php");
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo "$matricula"?></title>
-</head>
-<body>
-    <?php echo $nome; ?>
-</body>
-</html>
