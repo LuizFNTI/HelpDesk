91c5e87603522a212019faea9a3c1888f775df58
Luiz Felipe do Nascimento <felllipppe.nascimento@gmail.com>
2021-11-26 11:54:51 -0300

    Update listaChamadoAnalista.php

--- helpdesk/Frontend/listaChamadoAnalista.php
+++ helpdesk/Frontend/listaChamadoAnalista.php
@@ -13,7 +13,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title><?php echo $matricula ?></title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="CSS/listaChamadoAnalista.css">
 </head>
@@ -33,7 +33,7 @@
             <button type="button" class="btn btn-primary">Configurações</button>
         </li>
         <li class="nav-item" id="btsair">
-            <button type="button" class="btn btn-danger">Sair</button>
+            <a href="../Backend/logout.php">Sair</a>
         </li>
     </ul>
     </nav>
