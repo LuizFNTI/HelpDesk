16c60ae97fc7fece67a6876cd0090e5facab7bb0
Luiz Felipe do Nascimento <felllipppe.nascimento@gmail.com>
2022-01-03 11:30:51 -0300

    u

--- helpdesk/Frontend/fecharChamado.php
+++ helpdesk/Frontend/fecharChamado.php
@@ -41,29 +41,3 @@
         header("location: listaChamadoAnalista.php");
     }
 ?>
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ver Chamado</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="CSS/verChamadoAnalista.css">
-</head>
-<body>
-    <main class="row justify-content-center align-items-center">
-        <div id="dpc">
-            <form action="fecharChamado.php" method="POST">
-                <input type="hidden" name="vnc" value="<?php echo $resultado['numero_chamado']; ?>">
-                <input type="hidden" name="aberto" value="3">
-                <div class="form-group">
-                    <label for="descricao">Faça uma breve descrição sobre o encerramento:</label>
-                    <textarea class="form-control" rows="5" placeholder="Descrição Analista:" id="descr" name="descanalista"></textarea>
-                </div>
-                <input type="submit" value="Encerrar">
-            </form>
-        </div> <!--dpc-->
-    </main>
-</body>
-</html>
