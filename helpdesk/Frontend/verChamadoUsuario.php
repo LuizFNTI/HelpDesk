7b2b9d62f828cbe57f5d2d30b963349f82f50224
Luiz Felipe do Nascimento <felllipppe.nascimento@gmail.com>
2022-01-05 11:23:28 -0300

    Update verChamadoUsuario.php

--- helpdesk/Frontend/verChamadoUsuario.php
+++ helpdesk/Frontend/verChamadoUsuario.php
@@ -3,6 +3,8 @@
 
     if(isset($_SESSION['usuario']) && is_array($_SESSION['usuario'])) {
         $matricula = $_SESSION['usuario'][0];
+        $nivel = $_SESSION['usuario'][1];
+        $nome_usuario = $_SESSION['usuario'][2];
         $nome_analista = $_SESSION['usuario'][2];
     } else {
         header("location: ../index.php");
@@ -50,50 +52,153 @@
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
-    <meta charset="UTF-8">
+    <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ver Chamado Usuario</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="CSS/verChamadoUsuario.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>SB Admin 2 - Tables</title>
+
+    <!-- Custom fonts for this template -->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this page -->
+    <link href="vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+
 </head>
-<body>
-    <main class="row justify-content-center align-items-center">
-        <div id="dpc">
-            <div class="row">
-                <div class="col">
-                    <div id="numchamado">
-                        <p>Numero Chamado: <?php echo $resultado['numero_chamado']; ?></p>
-                    </div><br>
-                    <div id= "infodemanda">
-                        <div class="row">
-                        <p><?php echo $resultado['nome_tipo']; ?></p>>
-                        <p><?php echo $resultado['nome_categoria']; ?></p>>
-                        <p><?php echo $resultado['nome_subcategoria']; ?></p>>
-                        <p><?php echo $resultado['nome_item']; ?></p>
-                        </div> <!--row-->
-                    </div>
-                    <div id="usuario">
-                        <p><?php echo $resultado['analista']; ?></p>
-                    </div><br><br><br><br>
-                    <div id="descricao">
-                    <form action="verChamadoUsuario.php" method="POST">
-                        <div class="form-group">
-                        <label for="descricao">Edite a descrição da sua solicitação:</label>
-                        <textarea class="form-control" rows="5" placeholder="Descrição:" id="descr" name="descricao" value="<?php echo $resultado['descricao']; ?>"></textarea>
+
+<body id="page-top">
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+        <!-- Sidebar -->
+        <?php include 'navbar.php'; ?>
+        <!-- End of Sidebar -->
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+
+                <!-- Topbar -->
+                <?php include 'topbar.php'; ?>
+                <!-- End of Topbar -->
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col">
+                            <div class="container-sm">
+                                <p>Numero Chamado: <?php echo $resultado['numero_chamado']; ?></p>
+                            </div>
+                            <div class="container-sm">
+                                <p>Analista: <?php echo $resultado['analista']; ?></p>
+                            </div>
+                            <div class="container-sm">
+                                <!--Passa as informações para imprimir na tela-->
+                                <?php echo $resultado['nome_tipo'] . ">" 
+                                . $resultado['nome_categoria'] . ">" . $resultado['nome_subcategoria'] . ">" .$resultado['nome_item']; ?>
+                            </div><br>
+                        </div> <!--col-->
+                        <div class="col">
+                            <div class="container-sm">
+                                <p>Data e Hora abertura: <?php echo $resultado['data_hora_abertura']; ?></p>
+                                <p>Data Prazo: <?php if($resultado['data_prazo'] == null) {echo "Data prazo ainda não definida pelo analista! ";} else {echo $resultado['data_prazo'];} ?>
+                            </div>
                         </div>
-                        <input type="submit" value="Editar">
-                    </form>
+                    </div><br> <!--row-->
+                    <div class="row">
+                        <div class="container-sm">
+                            <form action="verChamadoUsuario.php" method="POST">
+                                <div class="form-group">
+                                    <label for="descricao">Edite a descrição da sua solicitação:</label>
+                                    <textarea class="form-control" rows="5" placeholder="Descrição:" id="descr" name="descricao"> <?php echo $resultado['descricao']; ?></textarea>
+                                </div>
+                                <input type="submit" value="Editar" class="btn btn-primary btn-block">
+                            </form>
+                        </div>
+                    </div><br>
+                    <div class="row">
+                        <div class="container-sm">
+                            <p>Resposta Analista: <?php echo $resultado['descricao_analista']; ?></p>
+                        </div>
                     </div>
-                </div> <!--col-->
-                <div class="col">
-                    <p>Data e Hora abertura: <?php echo $resultado['data_hora_abertura']; ?></p>
-                    <p>Data prazo: <?php echo $resultado['data_prazo']; ?></p><br><br><br><br><br>
-                    <p>Resposta Analista: <?php echo $resultado['descricao_analista']; ?></p>
                 </div>
-            </div> <!--row-->
-        </div> <!--dpc-->
-    </main>
+                <!-- /.container-fluid -->
+            </div>
+            <!-- End of Main Content -->
+
+            <!-- Footer -->
+            <footer class="sticky-footer bg-white">
+                <div class="container my-auto">
+                    <div class="copyright text-center my-auto">
+                        <span>Copyright &copy; Prefeitura Municipal de Telêmaco Borba</span>
+                    </div>
+                </div>
+            </footer>
+            <!-- End of Footer -->
+
+        </div>
+        <!-- End of Content Wrapper -->
+
+    </div>
+    <!-- End of Page Wrapper -->
+
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
+
+    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Sair?</h5>
+                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="modal-body">Tem Certeza que deseja sair?</div>
+                <div class="modal-footer">
+                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancelar</button>
+                    <a class="btn btn-primary" href="../Backend/logout.php">Logout</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="js/sb-admin-2.min.js"></script>
+
+    <!-- Page level plugins -->
+    <script src="vendor/datatables/jquery.dataTables.min.js"></script>
+    <script src="vendor/datatables/dataTables.bootstrap4.min.js"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="js/demo/datatables-demo.js"></script>
+
+    <script src="JS/JQuery/jquery-3.6.0.min.js"></script>
+    <script src="JS/ajaxCategoria.js"></script>
+    <script src="JS/ajaxSubCat.js"></script>
+    <script src="JS/ajaxItem.js"></script>
 </body>
+
 </html>
