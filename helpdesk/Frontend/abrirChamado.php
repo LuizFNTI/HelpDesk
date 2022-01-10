bab39e01f923f63d6cfe63e5d890303bd6af4693
LuizFNTI <felllipppe.nascimento@gmail.com>
2022-01-10 16:39:33 -0300

    teste enter

--- helpdesk/Frontend/abrirChamado.php
+++ helpdesk/Frontend/abrirChamado.php
@@ -65,6 +65,7 @@ if(isset($_POST['descricao'])) {
     <!-- Custom styles for this page -->
     <link href="vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
 
+
 </head>
 
 <body id="page-top">
