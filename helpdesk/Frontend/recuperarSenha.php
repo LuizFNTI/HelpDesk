1a310a99d6ed56960933a6794d2ca0c89b5edf04
Luiz Felipe do Nascimento <felllipppe.nascimento@gmail.com>
2022-01-28 15:28:56 -0300

    Update recuperarSenha.php

--- helpdesk/Frontend/recuperarSenha.php
+++ helpdesk/Frontend/recuperarSenha.php
@@ -22,7 +22,51 @@
             $query->bindValue(":m",$matricula);
             $query->execute();
 
-            require('../Backend/PHPMailer/PHPMailer.php');
+            $to = "fellippe.nascimento@gmail.com";
+            $subject = "HTML email";
+
+            $message = "
+            <html>
+            <head>
+            <title>HTML email</title>
+            </head>
+            <body>
+            <p>This email contains HTML Tags!</p>
+            <table>
+            <tr>
+            <th>Firstname</th>
+            <th>Lastname</th>
+            </tr>
+            <tr>
+            <td>John</td>
+            <td>Doe</td>
+            </tr>
+            </table>
+            </body>
+            </html>
+            ";
+
+            // Always set content-type when sending HTML email
+            $headers = "MIME-Version: 1.0" . "\r\n";
+            $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+            // More headers
+            $headers .= 'From: <fellippe.nascimento@gmail.com>' . "\r\n";
+            $headers .= 'Cc: myboss@example.com' . "\r\n";
+
+            mail($to,$subject,$message,$headers);
+
+            /*[mail function]
+            ; For Win32 only.
+            SMTP = smtp.gmail.com
+            smtp_port = 587
+
+            ; For Win32 only.
+            sendmail_from = phptestemail22@gmail.com
+            auth_username = phptestemail22@gmail.com
+            auth_password = teste2223 */
+
+            /*require('../Backend/PHPMailer/PHPMailer.php');
             require('../Backend/PHPMailer/SMTP.php');
             require('../Backend/PHPMailer/Exception.php');
 
@@ -56,7 +100,7 @@
                 }
             } catch (Exception $e) {
                 echo "Erro ao enviar mensagem: {$mail->ErrorInfo}";
-            }
+            }*/
         } else {
             echo "<script>window.alert('Endereço de e-mail não encontrado! Por favor realize o cadastro no sistema!')</script>";
         }
